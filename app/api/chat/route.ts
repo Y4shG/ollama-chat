1e671b4c88ba01880faba39753ca6862eb79510a
Hein Htoo <heinhtoozaw99@gmail.com>
2024-09-19 13:53:15 +0630

    update

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,5 @@
-import { ModelFusionTextStream, asChatMessages } from "@modelfusion/vercel-ai";
-import { Message } from "ai";
+import { asChatMessages, ModelFusionTextStream } from "@modelfusion/vercel-ai";
+import { Message, StreamingTextResponse } from "ai";
 import { ollama, streamText } from "modelfusion";
 
 export const runtime = "edge";
@@ -8,23 +8,38 @@ export async function POST(req: Request) {
   // useChat will send a JSON with a messages property:
   const { messages }: { messages: Message[] } = await req.json();
 
-  const model = ollama
-    .ChatTextGenerator({ model: "llama3.1" })
-    .withChatPrompt();
+  const textStream = await streamText({
+    model: ollama.ChatTextGenerator({ model: "llama3.1" }).withChatPrompt(),
+    prompt: {
+      system:
+        "You are an AI chat bot. " +
+        "Follow the user's instructions carefully.",
 
-  const prompt = {
-    system: "You are an AI chatbot. Follow the user's instructions carefully.",
-
-    // map Vercel AI SDK Message to ModelFusion ChatMessage:
-    messages: asChatMessages(messages),
-  };
-
-  const textStream = await streamText({ model, prompt });
-
-  const response = new Response(ModelFusionTextStream(textStream), {
-    status: 200,
+      // map Vercel AI SDK Message to ModelFusion ChatMessage:
+      messages: asChatMessages(messages),
+    },
   });
-  response.headers.set("Content-Type", "text/plain; charset=utf-8");
 
-  return response;
+  // Return the result using the Vercel AI SDK:
+  return new StreamingTextResponse(
+    // Convert the text stream to a Vercel AI SDK compatible stream:
+    ModelFusionTextStream(
+      textStream,
+      // optional callbacks:
+      {
+        onStart() {
+          console.log("onStart");
+        },
+        onToken(token) {
+          console.log("onToken", token);
+        },
+        onCompletion: () => {
+          console.log("onCompletion");
+        },
+        onFinal(completion) {
+          console.log("onFinal", completion);
+        },
+      }
+    )
+  );
 }
